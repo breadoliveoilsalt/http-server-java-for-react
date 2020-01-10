8be6472e23a2eb90c651f97a6ebbdd078486313e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-01-10 14:27:33 -0500

    Move acceptance tests to root directory. Add bash script to run

---
+++ acceptance_tests.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -Eeou pipefail
+
+function finish {
+  echo "Killing server process!"
+  kill "$SERVER_PID"
+}
+trap finish EXIT
+
+cd http_server_spec
+bundle install
+java -jar "../build/libs/$(ls ../build/libs)" &
+SERVER_PID=$!
+bundle exec spinach || true
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 class App {
 
     public static void main(String[] args) throws IOException {
-        int port = 8000;
+        int port = 5000;
         AppFactory factory = new ChatServerAppFactory();
 
         new ChatServerInit(port, factory).run();
