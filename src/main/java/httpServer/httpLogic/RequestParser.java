cba903fe399d6853352480f3b900b89629ef4888
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-01-14 12:25:30 -0500

    Pass third test for RequestParser

--- src/main/java/httpServer/httpLogic/RequestParser.java
+++ src/main/java/httpServer/httpLogic/RequestParser.java
@@ -11,9 +11,13 @@ public class RequestParser {
     public static Request parse(String rawClientRequest) {
         Request request = new Request();
         String[] metaDataAndBody = rawClientRequest.split("\r\n");
-        String[] requestLine = metaDataAndBody[0].split("\n");
-        String method = requestLine[0].split(" ")[0];
+        String[] parsedRequestLine = metaDataAndBody[0].split("\n")[0].split(" ");
+//        String method = requestLine[0].split(" ")[0];
+        String method = parsedRequestLine[0];
         request.setMethod(method);
+        String path = parsedRequestLine[1];
+        request.setPath(path);
+        // add html version
         return request;
     }
 }
