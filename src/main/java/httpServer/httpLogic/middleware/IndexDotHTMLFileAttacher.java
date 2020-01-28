fba29fc9725daec2105a97f885130c9c0bd39e74
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-01-28 17:34:04 -0500

    Add Middleware abstract class

---
+++ src/main/java/httpServer/httpLogic/middleware/IndexDotHTMLFileAttacher.java
@@ -0,0 +1,12 @@
+package httpServer.httpLogic.middleware;
+
+import httpServer.httpLogic.requests.Request;
+import httpServer.httpLogic.responses.Response;
+
+public class IndexDotHTMLFileAttacher extends Middleware {
+
+    @Override
+    public void handle(Request request, Response response) {
+
+    }
+}
