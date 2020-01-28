047d14424783cdafb29fef0b534579abe4e0ef59
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-01-28 11:34:08 -0500

    Pass tests for Logger

---
+++ src/main/java/httpServer/serverSocketLogic/serverLogger/ServerLogger.java
@@ -0,0 +1,76 @@
+package httpServer.serverSocketLogic.serverLogger;
+
+import httpServer.httpLogic.constants.Whitespace;
+import httpServer.httpLogic.requests.Request;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class ServerLogger {
+
+    private OutputStream outputStream;
+    private ArrayList<String> logList;
+
+    public ServerLogger(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        this.logList = new ArrayList<>();
+    }
+
+    public ArrayList<String> getLogList() {
+        return logList;
+    }
+
+    public ServerLogger print(String message) {
+        try {
+            logList.add(message);
+            writeToOutputStream(message);
+            return this;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return this;
+        }
+    }
+
+    private void writeToOutputStream(String message) throws IOException {
+        outputStream.write(message.getBytes());
+        outputStream.flush();
+    }
+
+    public void logServerInit(int port) {
+        try {
+            String message = "The server is now listening on port " + String.valueOf(port);
+            logList.add(message);
+            writeToOutputStream(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logServerShuttingDown() {
+        try {
+            String message = "The server is shutting down";
+            logList.add(message);
+            writeToOutputStream(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logClientConnection(Request request) {
+        try {
+            String hostName = request.getHeaderValue("Host");
+            String message =
+                    hostName +
+                    " made a " +
+                    request.getMethod() +
+                    " request to " +
+                    request.getPath();
+            logList.add(message);
+            writeToOutputStream(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/test/java/httpServerTests/serverSocketLogicTests/loggerTests/ServerLoggerTests.java
+++ src/test/java/httpServerTests/serverSocketLogicTests/loggerTests/ServerLoggerTests.java
@@ -1,13 +1,18 @@
 package httpServerTests.serverSocketLogicTests.loggerTests;
 
 import httpServer.httpLogic.constants.HTTPMethods;
+import httpServer.httpLogic.constants.Whitespace;
+import httpServer.httpLogic.requests.Request;
 import httpServer.httpLogic.requests.RequestBuilder;
+import httpServer.serverSocketLogic.serverLogger.ServerLogger;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ServerLoggerTests {
@@ -23,24 +28,30 @@ public class ServerLoggerTests {
 
     @Test
     public void printSendsAStringToTheSpecifiedOutputStream() {
+        String message = "Message 1";
         assertTrue(logger.getLogList().isEmpty());
-        logger.print("Message 1");
+        logger.print(message);
+
+        assertEquals(message, outputStream.toString());
 
         assertTrue(logger.getLogList().contains("Message 1"));
     }
 
     @Test
-    public void printCanBeChained() {
+    public void printCanBeChained() throws IOException {
+        String message1 = "Message 1";
+        String message2 = "Message 2";
         assertTrue(logger.getLogList().isEmpty());
-        logger.print("Message 1").print("Message 2");
+        logger.print(message1).print(message2);
 
+        assertEquals(message1 + message2, outputStream.toString());
         assertTrue(logger.getLogList().contains("Message 1"));
         assertTrue(logger.getLogList().contains("Message 2"));
     }
 
     @Test
     public void logServerInitLogsAMessageThatTheServerStarted() {
-        logger.logServerInit("5000");
+        logger.logServerInit(5000);
 
         String expectedLog = "The server is now listening on port 5000";
         assertTrue(logger.getLogList().contains(expectedLog));
@@ -56,7 +67,7 @@ public class ServerLoggerTests {
 
     @Test
     public void logClientConnectionLogsTheConnectedHostAndItsMethodAndResourceRequested() {
-        Request clientRequest = new RequestBuilder().addMethod(HTTPMethods.GET).addPath("/simple_get").addHeader("Host", "000.000.000");
+        Request clientRequest = new RequestBuilder().addMethod(HTTPMethods.GET).addPath("/simple_get").addHeader("Host", "000.000.000").build();
 
         logger.logClientConnection(clientRequest);
 
