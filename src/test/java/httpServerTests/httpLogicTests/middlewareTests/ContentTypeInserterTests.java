325bca0bdc54c0410006995ec885a6a4d6066a5a
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-03 13:05:40 -0500

    Add failing test for ContentTypeInserter on calling next middleware

---
+++ src/test/java/httpServerTests/httpLogicTests/middlewareTests/ContentTypeInserterTests.java
@@ -0,0 +1,41 @@
+package httpServerTests.httpLogicTests.middlewareTests;
+
+import httpServer.httpLogic.constants.HTTPMethods;
+import httpServer.httpLogic.requests.Request;
+import httpServer.httpLogic.requests.RequestBuilder;
+import httpServer.httpLogic.responses.Response;
+import httpServerTests.httpLogicTests.testRouterAndControllers.TestPaths;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.junit.Assert.assertTrue;
+
+public class ContentTypeInserterTests {
+
+    private Request request;
+    private Response response;
+    private ContentTypeInserter contentTypeInserter;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Before
+    public void testInit() {
+        request = new RequestBuilder().build();
+        response = new Response();
+        contentTypeInserter = new ContentTypeInserter();
+    }
+
+    @Test
+    public void handleCallsTheNextMiddlewareInTheChainIfOneExists() {
+        MockMiddleware nextMiddleware = new MockMiddleware();
+        contentTypeInserter.setNext(nextMiddleware);
+
+        contentTypeInserter.handle(request, response);
+
+        assertTrue(nextMiddleware.handleWasCalled);
+    }
+
+}
