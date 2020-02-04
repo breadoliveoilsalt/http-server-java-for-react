d70ab70662ddad4d8e7f283258605075d17541f7
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-04 16:32:19 -0500

    Pass tests for RequestValidator on paths with two dots

--- src/main/java/httpServer/httpLogic/middleware/RequestValidator.java
+++ src/main/java/httpServer/httpLogic/middleware/RequestValidator.java
@@ -5,14 +5,24 @@ import httpServer.httpLogic.requests.Request;
 import httpServer.httpLogic.responses.Response;
 import httpServer.router.Router;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RequestValidator extends Middleware {
 
     private final Router router;
     private Request request;
     private Response response;
+    private Set<String> blacklistedResources;
 
     public RequestValidator(Router router) {
         this.router = router;
+        populateBlacklistedResources();
+    }
+
+    private void populateBlacklistedResources() {
+        blacklistedResources = new HashSet<>();
+        blacklistedResources.add("..");
     }
 
     @Override
@@ -30,6 +40,8 @@ public class RequestValidator extends Middleware {
             response.statusCode = HTTPStatusCodes.BadRequest;
         } else if (requestHasUnrecognizedMethod()) {
             response.statusCode = HTTPStatusCodes.NotImplemented;
+        } else if (pathRequestedContainsBlacklistedResource()) {
+            response.statusCode = HTTPStatusCodes.NotFound;
         }
     }
 
@@ -41,5 +53,19 @@ public class RequestValidator extends Middleware {
         return !requestHasRecognizedMethod();
     }
 
+    private boolean pathRequestedContainsBlacklistedResource() {
+        String[] requestPaths = request.getPath().split("/");
+        return pathRequestedContainsBlacklistedResource(requestPaths);
+   }
+
+    private boolean pathRequestedContainsBlacklistedResource(String[] requestPaths) {
+        boolean pathBlacklisted = false;
+        for (String path : requestPaths) {
+            if (blacklistedResources.contains(path)) {
+                pathBlacklisted = true;
+            }
+        }
+        return pathBlacklisted;
+    }
 
 }
